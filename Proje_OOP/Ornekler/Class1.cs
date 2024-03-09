c38efaa6899eeb627a13a32c63d2183931518f8f
Begüm Yünkül <yunkulbegum@gmail.com>
2024-03-09 13:53:35 +0300

    sınıflar ve property'ler tanımlandı.

---
+++ Proje_OOP/Ornekler/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Proje_OOP.Entity;
+
+namespace Proje_OOP.Ornekler
+{
+    //siniflarin icindeki ogelere erisim saglamak icin siniftan bir nesne turetilir.
+    //SınıfAdı nesneadı=new SınıfAdı();
+    public class Class1
+    {
+        Product product = new Product();
+    }
+}
+//Product --> sınıf (class)
+//product --> nesne (onject)
